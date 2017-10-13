132340f46d65522fa50cfefc12045e68f7799534
unknown <yogevcohen@gmail.com>
2017-10-13 10:16:25 +0300

    added all

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log('Failed writing log file', err);
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintaince.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('hello express!');
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        wellcomeMessage: 'Wellcome',
+    });
+});
+app.get('/about', (req, res) => {
+    // res.send('hello express!');
+    res.render('about.hbs', {
+        pageTitle: 'About',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    // res.send('hello express!');
+    res.send({
+        errorMessage: 'Failed'
+    });
+});
+app.listen(3000, () => {
+    console.log('Server up');
+});
