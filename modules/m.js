4f67f9bf89d8228f54ae74c99cda41a7a15e515b
unknown <yogevcohen@gmail.com>
2017-10-14 18:24:06 +0300

    ddd

--- modules/m.js
+++ modules/m.js
@@ -1,10 +1,12 @@
 
-getAllCalendarDays =  () => {
-    console.log('dd');
+calcHeatOnListings = async (litingList) => {
+    litingList.map((listing) => {
+        listing.heat = (listing.price / 20) * listing.demand;
+    });
+    return litingList;
 }
 
 
-
 module.exports = {
-    getAllCalendarDays,
+    calcHeatOnListings,
 };
